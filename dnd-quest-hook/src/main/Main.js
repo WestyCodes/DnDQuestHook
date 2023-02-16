2cb3558cb5273eb187146040af06605a0a116f88
Oliver West <oliversimon.west@gmail.com>
2023-02-16 10:54:43 +0000

    random logic completed and conditional rendering for quest text and generate quest text completed

--- dnd-quest-hook/src/main/Main.js
+++ dnd-quest-hook/src/main/Main.js
@@ -18,7 +18,8 @@ export default function Main() {
     const [obstacleObjectsMotivation, setObstacleObjectsMotivation] = useState([])
 
     const [questText, setQuestText] = useState()
-    const [loading, setIsLoading] = useState(true)
+    const [loadingAPI, setIsLoadingAPI] = useState(true)
+    const [loadQuest, setLoadQuest] = useState(false)
 
     
     async function promptsAPIReuqest() {
@@ -54,16 +55,17 @@ export default function Main() {
 
 
     useEffect(() => {
-        setIsLoading(true)
+        setIsLoadingAPI(true)
         async function getMyPrompts() {
             const res = await promptsAPIReuqest()
-            setIsLoading(false)
+            setIsLoadingAPI(false)
             return res
         }
         getMyPrompts()
     }, [])
 
     const randomiseQuest = () => {
+        setLoadQuest(false)
         const quest = {}
         quest.mainLocation = broadLocations[Math.floor(Math.random()*broadLocations.length)].title
         quest.inhabitantsDescription = raceDescription[Math.floor(Math.random()*raceDescription.length)].title
@@ -72,7 +74,8 @@ export default function Main() {
         quest.cultureReason = activityReason[Math.floor(Math.random()*activityReason.length)].title
         quest.pcLocationAdj = specificLocationAdjective[Math.floor(Math.random()*specificLocationAdjective.length)].title
         quest.pcLocation = specificLocation[Math.floor(Math.random()*specificLocation.length)].title
-        quest.pcMood = specificCharacterState[Math.floor(Math.random()*specificCharacterState.length)].title
+        quest.pcMood1 = specificCharacterState[Math.floor(Math.random()*specificCharacterState.length)].title
+        quest.pcMood2 = specificCharacterState[Math.floor(Math.random()*specificCharacterState.length)].title
         quest.questAction = taskAction[Math.floor(Math.random()*taskAction.length)].title
         quest.questObject = taskObject[Math.floor(Math.random()*taskObject.length)].title
         quest.goalAction = specificGoalAction[Math.floor(Math.random()*specificGoalAction.length)].title
@@ -80,6 +83,7 @@ export default function Main() {
         quest.antagonistObject = obstacleObject[Math.floor(Math.random()*obstacleObject.length)].title
         quest.antagonistMotivation = obstacleObjectsMotivation[Math.floor(Math.random()*obstacleObjectsMotivation.length)].title
         setQuestText(quest)
+        setLoadQuest(true)
         // console.log(questText)
     }
 
@@ -89,13 +93,18 @@ export default function Main() {
         <div>
             <p>This is the Main</p>
             <p>This is a test randomised Quest:</p>
-            {loading ? (<p>Loading...</p>) : (<p>
-                {broadLocations[0]?.title}, where {raceDescription[0]?.title} {races[0]?.title} {cultureActivity[0]?.title} {activityReason[0]?.title}, 
-                you find yourself in a {specificLocationAdjective[0]?.title} {specificLocation[0]?.title}, {specificCharacterState[0]?.title} and {specificCharacterState[0]?.title}. 
-                You must {taskAction[0]?.title} the {taskObject[0]?.title} in order to {specificGoalAction[0]?.title} {specificGoalObject[0]?.title}.
-                However, the {obstacleObject[0]?.title} would be opposed to the {taskObject[0]?.title} {obstacleObjectsMotivation[0]?.title}!
-            </p>)}
-            <p onClick={(randomiseQuest)}>That sucks, try again!</p>
+            {loadQuest && (
+                <div>
+                    <p>
+                        {questText.mainLocation}, where {questText.inhabitantsDescription} {questText.inhabitants} {questText.culture} {questText.cultureReason}, 
+                        you find yourself in a {questText.pcLocationAdj} {questText.pcLocation}, {questText.pcMood1} and {questText.pcMood2}. 
+                        You must {questText.questAction} the {questText.questObject} in order to {questText.goalAction} {questText.goalObject}.
+                        However, the {questText.antagonistObject} would be opposed to the {questText.questObject} {questText.antagonistMotivation}!
+                    </p>
+                </div>
+            )}
+            {loadingAPI ? (<p>Loading...</p>) : (<p onClick={(randomiseQuest)}>{loadQuest ? `That sucks... do it again!`: `Generate Me A Quest!`}</p>)}
+            {/* <p onClick={(randomiseQuest)}>That sucks, try again!</p> */}
         </div>
     )
 }
